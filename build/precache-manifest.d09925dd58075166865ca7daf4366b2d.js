07fcc1fc1dc987a3faf9a5f0bbb512f4477af3f8
lilit.tadevosyan <lilit.tadevosyan@sflpro.com>
2019-07-27 19:22:44 +0400

    Add build folder to check deployment

---
+++ build/precache-manifest.d09925dd58075166865ca7daf4366b2d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b221cb7dd01f129737482dad0b5ebe1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79fb82fe0b8c1be06a9a",
+    "url": "/static/css/main.24bcfac6.chunk.css"
+  },
+  {
+    "revision": "e36713b6381decbbf36b",
+    "url": "/static/js/2.8e94af23.chunk.js"
+  },
+  {
+    "revision": "79fb82fe0b8c1be06a9a",
+    "url": "/static/js/main.5d7a4e48.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f0ec5079a163fe9bc5ffa98e77ec8638",
+    "url": "/static/media/bg.f0ec5079.png"
+  },
+  {
+    "revision": "9d2f9e3e298a877cc6908686e0a89699",
+    "url": "/static/media/logo.9d2f9e3e.png"
+  }
+]);
